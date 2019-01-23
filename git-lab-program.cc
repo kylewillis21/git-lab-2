889c4a9fe1a5f9d86b4d95245563c82032ff0376
kylewillis21 <kw586717@ohio.edu>
2019-01-22 19:05:36 -0500

    Initial program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Kyle Willis
+ *        Date: January 22, 2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << end1;
+    return 0;
+}// main
